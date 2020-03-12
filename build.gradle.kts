1d3109d13ad1b5a30282586a668ceaa0391574b0
Jared Woolston <jwoolston@idealcorp.com>
2020-03-12 13:24:04 -0700

    Temporarily disables native targets.
    Signed-off-by: Jared Woolston <Jared.Woolston@gmail.com>

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    id("org.jetbrains.kotlin.multiplatform") version "1.3.61" apply false
+}
+
+allprojects {
+    group = "org.aquila3d"
+    version = "0.1.0-SNAPSHOT"
+
+    repositories {
+        mavenCentral()
+        jcenter()
+        maven { setUrl("http://dl.bintray.com/kotlin/kotlinx.html/") }
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+enableFeaturePreview("GRADLE_METADATA")
+
+/*pluginManagement {
+    val detekt_version: String by settings
+
+    plugins {
+        id("io.gitlab.arturbosch.detekt") version detekt_version
+    }
+}*/
+
+include(":core")
