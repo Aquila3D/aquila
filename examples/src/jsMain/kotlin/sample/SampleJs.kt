bd29518ad14f5e4fd0d552b9cae9afef0c64f501
Jared Woolston <Jared.Woolston@gmail.com>
2020-03-13 10:00:26 -0700

    Having trouble getting basic JS target to load.
    Signed-off-by: Jared Woolston <Jared.Woolston@gmail.com>

--- examples/src/jsMain/kotlin/sample/SampleJs.kt
+++ examples/src/jsMain/kotlin/sample/SampleJs.kt
@@ -1,13 +1,5 @@
 package sample
 
-actual class Sample {
-    actual fun checkMe() = 12
-}
-
-actual object Platform {
-    actual val name: String = "JS"
-}
-
 fun main(args: Array<String>) {
     println("Hello JavaScript!")
 }
