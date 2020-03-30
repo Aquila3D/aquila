422767fd4cc7079588de5292c249b290a4a5aaa7
Jared Woolston <Jared.Woolston@gmail.com>
2020-03-29 20:18:52 -0700

    Some basic documentation.
    Signed-off-by: Jared Woolston <Jared.Woolston@gmail.com>

--- core/src/commonMain/kotlin/org/aquila3d/core/vulkan/VkAttachmentLoadOp.kt
+++ core/src/commonMain/kotlin/org/aquila3d/core/vulkan/VkAttachmentLoadOp.kt
@@ -1,7 +1,30 @@
 package org.aquila3d.core.vulkan
 
+/**
+ * Specify how contents of an attachment are treated at the beginning of a subpass.
+ */
 enum class VkAttachmentLoadOp(val value: Int) {
+
+    /**
+     * Specifies that the previous contents of the image within the render area will be preserved. For attachments with
+     * a depth/stencil format, this uses the access type [VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_READ_BIT]. For attachments
+     * with a color format, this uses the access type [VK_ACCESS_COLOR_ATTACHMENT_READ_BIT].
+     */
     VK_ATTACHMENT_LOAD_OP_LOAD(0),
+
+    /**
+     * Specifies that the contents within the render area will be cleared to a uniform value, which is specified when a
+     * render pass instance is begun. For attachments with a depth/stencil format, this uses the access type
+     * [VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT]. For attachments with a color format, this uses the access type
+     * [VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT].
+     */
     VK_ATTACHMENT_LOAD_OP_CLEAR(1),
+
+    /**
+     * Specifies that the previous contents within the area need not be preserved; the contents of the attachment will
+     * be undefined inside the render area. For attachments with a depth/stencil format, this uses the access type
+     * [VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT]. For attachments with a color format, this uses the access type
+     * [VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT].
+     */
     VK_ATTACHMENT_LOAD_OP_DONT_CARE(2)
 }
--- core/src/commonMain/kotlin/org/aquila3d/core/vulkan/command/VkCommandBuffer.kt
+++ core/src/commonMain/kotlin/org/aquila3d/core/vulkan/command/VkCommandBuffer.kt
@@ -2,11 +2,12 @@ package org.aquila3d.core.vulkan.command
 
 import org.aquila3d.core.vulkan.device.VkDevice
 
-enum class VkCommandBufferLevel(val value: Int) {
-    VK_COMMAND_BUFFER_LEVEL_PRIMARY(0),
-    VK_COMMAND_BUFFER_LEVEL_SECONDARY(1)
-}
-
+/**
+ * Command buffers are objects used to record commands which can be subsequently submitted to a device queue for
+ * execution. There are two levels of command buffers - primary command buffers, which can execute secondary command
+ * buffers, and which are submitted to queues, and secondary command buffers, which can be executed by primary command
+ * buffers, and which are not directly submitted to queues.
+ */
 expect class VkCommandBuffer(level: VkCommandBufferLevel, pool: VkCommandPool, device: VkDevice) {
 
 }
