80289aef56ae3224874027ea80d7b9926130cd28
Jared Woolston <Jared.Woolston@gmail.com>
2020-04-01 10:00:01 -0700

    Adds a sample builder DSL for initial review.
    Signed-off-by: Jared Woolston <Jared.Woolston@gmail.com>

--- core/src/commonMain/kotlin/org/aquila3d/core/vulkan/VkAttachmentDescription.kt
+++ core/src/commonMain/kotlin/org/aquila3d/core/vulkan/VkAttachmentDescription.kt
@@ -2,44 +2,100 @@ package org.aquila3d.core.vulkan
 
 class VkAttachmentDescription {
 
-    var format: VkFormat = VkFormat.VK_FORMAT_B8G8R8A8_UNORM
-    var samples: VkSampleCountFlagBits = VkSampleCountFlagBits.VK_SAMPLE_COUNT_1_BIT
-    var loadOp: VkAttachmentLoadOp = VkAttachmentLoadOp.VK_ATTACHMENT_LOAD_OP_CLEAR
-    var storeOp: VkAttachmentStoreOp = VkAttachmentStoreOp.VK_ATTACHMENT_STORE_OP_STORE
-    var stencilLoadOp: VkAttachmentLoadOp = VkAttachmentLoadOp.VK_ATTACHMENT_LOAD_OP_DONT_CARE
-    var stencilStoreOp: VkAttachmentStoreOp = VkAttachmentStoreOp.VK_ATTACHMENT_STORE_OP_DONT_CARE
-    var initialLayout: VkImageLayout = VkImageLayout.VK_IMAGE_LAYOUT_UNDEFINED
-    var finalLayout: VkImageLayout = VkImageLayout.VK_IMAGE_LAYOUT_PRESENT_SRC_KHR
+    val format: VkFormat
+    val samples: VkSampleCountFlagBits
+    val loadOp: VkAttachmentLoadOp
+    val storeOp: VkAttachmentStoreOp
+    val stencilLoadOp: VkAttachmentLoadOp
+    val stencilStoreOp: VkAttachmentStoreOp
+    val initialLayout: VkImageLayout
+    val finalLayout: VkImageLayout
 
-    inline fun format(format: () -> VkFormat) {
-        this.format = format()
+    private constructor(
+        format: VkFormat,
+        samples: VkSampleCountFlagBits,
+        loadOp: VkAttachmentLoadOp,
+        storeOp: VkAttachmentStoreOp,
+        stencilLoadOp: VkAttachmentLoadOp,
+        stencilStoreOp: VkAttachmentStoreOp,
+        initialLayout: VkImageLayout,
+        finalLayout: VkImageLayout
+    ) {
+        this.format = format
+        this.samples = samples
+        this.loadOp = loadOp
+        this.storeOp = storeOp
+        this.stencilLoadOp = stencilLoadOp
+        this.stencilStoreOp = stencilStoreOp
+        this.initialLayout = initialLayout
+        this.finalLayout = finalLayout
     }
 
-    inline fun samples(samples: () -> VkSampleCountFlagBits) {
-        this.samples = samples()
-    }
+    @VkAttachmentDescriptionDslMarker
+    companion object Builder {
 
-    inline fun loadOp(loadOp: () -> VkAttachmentLoadOp) {
-        this.loadOp = loadOp()
-    }
+        var format: VkFormat = VkFormat.VK_FORMAT_B8G8R8A8_UNORM
+        var samples: VkSampleCountFlagBits = VkSampleCountFlagBits.VK_SAMPLE_COUNT_1_BIT
+        var loadOp: VkAttachmentLoadOp = VkAttachmentLoadOp.VK_ATTACHMENT_LOAD_OP_CLEAR
+        var storeOp: VkAttachmentStoreOp = VkAttachmentStoreOp.VK_ATTACHMENT_STORE_OP_STORE
+        var stencilLoadOp: VkAttachmentLoadOp = VkAttachmentLoadOp.VK_ATTACHMENT_LOAD_OP_DONT_CARE
+        var stencilStoreOp: VkAttachmentStoreOp = VkAttachmentStoreOp.VK_ATTACHMENT_STORE_OP_DONT_CARE
+        var initialLayout: VkImageLayout = VkImageLayout.VK_IMAGE_LAYOUT_UNDEFINED
+        var finalLayout: VkImageLayout = VkImageLayout.VK_IMAGE_LAYOUT_PRESENT_SRC_KHR
 
-    inline fun storeOp(storeOp: () -> VkAttachmentStoreOp) {
-        this.storeOp = storeOp()
-    }
+        inline fun format(format: () -> VkFormat) {
+            this.format = format()
+        }
 
-    inline fun stencilLoadOp(loadOp: () -> VkAttachmentLoadOp) {
-        this.stencilLoadOp = loadOp()
-    }
+        inline fun samples(samples: () -> VkSampleCountFlagBits) {
+            this.samples = samples()
+        }
 
-    inline fun stencilStoreOp(storeOp: () -> VkAttachmentStoreOp) {
-        this.stencilStoreOp = storeOp()
-    }
+        inline fun loadOp(loadOp: () -> VkAttachmentLoadOp) {
+            this.loadOp = loadOp()
+        }
 
-    inline fun initialLayout(layout: () -> VkImageLayout) {
-        this.initialLayout = layout()
-    }
+        inline fun storeOp(storeOp: () -> VkAttachmentStoreOp) {
+            this.storeOp = storeOp()
+        }
 
-    inline fun finalLayout(layout: () -> VkImageLayout) {
-        this.finalLayout = layout()
+        inline fun stencilLoadOp(loadOp: () -> VkAttachmentLoadOp) {
+            this.stencilLoadOp = loadOp()
+        }
+
+        inline fun stencilStoreOp(storeOp: () -> VkAttachmentStoreOp) {
+            this.stencilStoreOp = storeOp()
+        }
+
+        inline fun initialLayout(layout: () -> VkImageLayout) {
+            this.initialLayout = layout()
+        }
+
+        inline fun finalLayout(layout: () -> VkImageLayout) {
+            this.finalLayout = layout()
+        }
+
+        fun build(): VkAttachmentDescription = VkAttachmentDescription(
+            format,
+            samples,
+            loadOp,
+            storeOp,
+            stencilLoadOp,
+            stencilStoreOp,
+            initialLayout,
+            finalLayout
+        )
+
+        operator fun invoke(): Builder {
+            return this
+        }
     }
 }
+
+fun attachmentDescription(lambda: VkAttachmentDescription.Builder.() -> Unit)
+        = VkAttachmentDescription.Builder()
+        .apply(lambda)
+        .build()
+
+@DslMarker
+annotation class VkAttachmentDescriptionDslMarker
